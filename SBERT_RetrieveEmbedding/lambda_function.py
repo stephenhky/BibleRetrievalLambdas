0c87c635bad8db7e288748a4c9a18f91999b5d37
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-07-07 18:59:05 -0400

    copy file from S3 to lambda.... not working

--- SBERT_RetrieveEmbedding/lambda_function.py
+++ SBERT_RetrieveEmbedding/lambda_function.py
@@ -1,8 +1,13 @@
 
 import json
 import os
+import tarfile
 
+import boto3
 from sentence_transformers import SentenceTransformer
+
+
+s3_bucket = 'huggingfacesentencebert'
 
 
 def lambda_handler(event, context):
@@ -11,10 +16,17 @@ def lambda_handler(event, context):
     text = query['text']
     sbertmodel = query['sbertmodel']
 
+    # copying models from s3
+    docker_model_path = os.path.join('/', 'tmp', sbertmodel)
+    s3 = boto3.resource('s3')
+    s3.meta.client.download_file(s3_bucket, sbertmodel+'.tar.gz', docker_model_path+'.tar.gz')
+    model_tar = tarfile.open(docker_model_path+'.tar.gz')
+    model_tar.extractall(os.path.join('/', 'tmp'))
+
     # loading SentenceBERT model
-    model = SentenceTransformer(os.path.join('/', 'models', sbertmodel))
+    model = SentenceTransformer(docker_model_path)
     embedding = model.encode([text])
-    embedding = embedding[0]
+    embedding = embedding[0]c
 
     # return
     return {
--- SBERT_RetrieveEmbedding/save_sbert_model.py
+++ SBERT_RetrieveEmbedding/save_sbert_model.py
@@ -1,20 +1,35 @@
 
+import os
+import tarfile
 from argparse import ArgumentParser
 
 from sentence_transformers import SentenceTransformer
 
 
+# reference: https://colab.research.google.com/drive/1eyVi8tkCr7N-sE-yyhDB_lduowp1EZ78?usp=sharing#scrollTo=IgBWFbGK0h9T
+def pack_model(model_path, tar_file_name):
+    root, dirs, files = list(os.walk(model_path))[0]
+    with tarfile.open(tar_file_name, 'w:gz') as f:
+        for file in files:
+            f.add(f'{model_path}/{file}', arcname=file)
+        for dir in dirs:
+            f.add(f'{model_path}/{dir}', arcname=dir)
+
+
 def get_argparser():
     argparser = ArgumentParser(description='Saving SentenceBERT model to a desginated path')
     argparser.add_argument('modelname', help='model name')
-    argparser.add_argument('outputdir', help='output directory')
+    argparser.add_argument('outputname', help='output name')
+    argparser.add_argument('--pack', default=False, action='store_true', help='pack model (default: False)')
     return argparser
 
 
 if __name__ == '__main__':
     args = get_argparser().parse_args()
     modelname = args.modelname
-    outputdir = args.outputdir
+    outputname = args.outputname
+    topack = args.pack
 
     model = SentenceTransformer(modelname)
-    model.save(outputdir)
+    model.save(outputname)
+    pack_model(outputname, outputname+'.tar.gz')
